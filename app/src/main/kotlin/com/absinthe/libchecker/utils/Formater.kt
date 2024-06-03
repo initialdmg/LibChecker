34e44d95bf52bd1c8e8d5b8410992ca81a7aba1e
dingmiaoguo <501190238@qq.com>
2024-06-03 21:13:59 +0800

    format

--- app/src/main/kotlin/com/absinthe/libchecker/utils/Formater.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/utils/Formater.kt
@@ -1,52 +1,57 @@
 package com.absinthe.libchecker.utils
 
-import java.util.Locale
 import android.content.Context;
+import java.util.Locale
 
-fun formatFileSize(context: Context, roundedBytes: Long): String {
-  return formatFileSize(roundedBytes, false, Locale.US)
-}
-
-private fun formatFileSize(roundedBytes: Long, shorter: Boolean, locale: Locale): String {
-  var result = roundedBytes.toFloat()
-  var suffix = "B"
-  if (result > 900) {
-    suffix = "KB"
-    result = result / 1024
-  }
-  if (result > 900) {
-    suffix = "MB"
-    result = result / 1024
-  }
-  if (result > 900) {
-    suffix = "GB"
-    result = result / 1024
-  }
-  if (result > 900) {
-    suffix = "TB"
-    result = result / 1024
-  }
-  if (result > 900) {
-    suffix = "PB"
-    result = result / 1024
-  }
-  val value: String
-  if (result < 1) {
-    value = String.format(locale, "%.2f", result)
-  } else if (result < 10) {
-    if (shorter) {
-      value = String.format(locale, "%.1f", result)
-    } else {
-      value = String.format(locale, "%.2f", result)
+object FileUtils {
+  fun formatFileSize(context: Context, roundedBytes: Long): String {
+    if (context == null) {
+      // nothing to do
     }
-  } else if (result < 100) {
-    if (shorter) {
+    return formatFileSize(roundedBytes, false, Locale.US)
+  }
+
+  private fun formatFileSize(roundedBytes: Long, shorter: Boolean, locale: Locale): String {
+    var result = roundedBytes.toFloat()
+    var suffix = "B"
+    if (result > 900) {
+      suffix = "KB"
+      result = result / 1024
+    }
+    if (result > 900) {
+      suffix = "MB"
+      result = result / 1024
+    }
+    if (result > 900) {
+      suffix = "GB"
+      result = result / 1024
+    }
+    if (result > 900) {
+      suffix = "TB"
+      result = result / 1024
+    }
+    if (result > 900) {
+      suffix = "PB"
+      result = result / 1024
+    }
+    val value: String
+    if (result < 1) {
+      value = String.format(locale, "%.2f", result)
+    } else if (result < 10) {
+      if (shorter) {
+        value = String.format(locale, "%.1f", result)
+      } else {
+        value = String.format(locale, "%.2f", result)
+      }
+    } else if (result < 100) {
+      if (shorter) {
+        value = String.format(locale, "%.0f", result)
+      } else {
+        value = String.format(locale, "%.2f", result)
+      }
+    } else {
       value = String.format(locale, "%.0f", result)
-    } else {
-      value = String.format(locale, "%.2f", result)
     }
-  } else {
-    value = String.format(locale, "%.0f", result)
+    return String.format("%s%s", value, suffix)
   }
-  return String.format("%s%s", value, suffix)
 }
